c9d1ae1f9d17a25c5c138609552860299e58ecf5
Julian Norton <Julian@JulianNorton.com>
2017-11-29 18:19:35 -0500

    maze 0 solution solver

--- ktaned/mazes.py
+++ ktaned/mazes.py
@@ -1,92 +1,61 @@
-import numpy as np
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
-a = np.eye(6, dtype=object)
+# maze0.svg
+# 0 is passable, 1 is wall
 
-#COORDINATE, POSSIBLE MOVES
-# row one
-a[0,0] = (2,3)
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+matrix = [
+    [0,0,0,0,0,1,0,0,0,0,0],
+    [0,1,1,1,0,1,0,1,1,1,1],
+    [0,1,0,0,0,1,0,0,0,0,0],
+    [0,1,0,1,1,1,1,1,1,1,0],
+    [0,1,0,0,0,1,0,0,0,0,0],
+    [0,1,1,1,0,1,0,1,1,1,0],
+    [0,1,0,0,0,0,0,1,0,0,0],
+    [0,1,1,1,1,1,1,1,1,1,0],
+    [0,0,0,0,0,1,0,0,0,1,0],
+    [0,1,1,1,0,1,0,1,1,1,0],
+    [0,0,0,1,0,0,0,1,0,0,0]
+]
 
-# row two
-a[0,0] = ('')
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+grid = Grid(matrix=matrix)
 
-# row three
-a[0,0] = ('')
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+start = grid.node(0, 0)
+end = grid.node(10, 10)
 
-# row four
-a[0,0] = ('')
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+path, runs = finder.find_path(start, end, grid)
 
-# row five
-a[0,0] = ('')
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+print('operations:', runs, 'path length:', len(path))
+print(grid.grid_str(path=path, start=start, end=end))
+solution = []
 
-# row six
-a[0,0] = ('')
-a[0,1] = ('')
-a[0,2] = ('')
-a[0,3] = ('')
-a[0,4] = ('')
-a[0,5] = ('')
+for i in range(len(path)):
+    if i%2 == 0:
+        print(i, 'can be divded by 2')
+        solution.append(path[i])
+    else:
+        print('sad!', i, 'cannot')
 
-print(a)
+print(path, 'becomes \n')
+print(solution)
 
+# print(i)
+def which_direction(solution):
+    solution_direction = []
+    for i in range(len(solution)-1):
+        current_location = solution[i]
+        next_location = solution[i+1]
+        if current_location[0] < next_location[0]:
+            print('move right!')
+        elif current_location[0] > next_location[0]:
+            print('move left!')
+        elif current_location[1] < next_location[1]:
+            print('move down!')
+        elif current_location[1] > next_location[1]:
+            print('move up!')
+        else:
+            'something went wrong'
 
-
-
-# (x) row, (y) column
-maze_option_shit ='''
-|───────────────────────|
-| 1   1   1 X 1   1   1 |
-|   xxXxx   x   xxXxxxX |
-| 1 x 1   1 X 1   1   1 |
-|   x   xxXxXxXxxxXxx   |
-| 1 X 1   1 X 1   1   1 |
-|   xxXxx   x   xxXxx   |
-| 1 X 1   1   1 X 1   1 |
-|   xxXxxxXxxxXxxxXxx   |
-| 1   1   1 X 1   1 X 1 |
-|   xxXxx   x   xxXxx   |
-| 1   1 X 1   1 X 1   1 |
-|───────────────────────|
-'''
-
-maze_option_2 ='''21212021212
-00100010100
-21202120212
-10001000001
-20212021212
-10100010001
-21202120202
-10001000101
-20202021202
-10101010001
-20212021212
-'''
-
-
-# [(0,0)(1,0,1,1)]
-
-
+which_direction(solution)
